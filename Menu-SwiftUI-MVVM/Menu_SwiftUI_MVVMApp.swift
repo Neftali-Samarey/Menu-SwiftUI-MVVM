b054cad7479ca8fa8746c09aae3356b9215fc4b3
Neftali-Samarey <neftali.quintero@cix.csi.cuny.edu>
2022-06-22 14:45:17 -0400

    Initial Commit

---
+++ Menu-SwiftUI-MVVM/Menu_SwiftUI_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Menu_SwiftUI_MVVMApp.swift
+//  Menu-SwiftUI-MVVM
+//
+//  Created by Neftali Samarey on 6/22/22.
+//
+
+import SwiftUI
+
+@main
+struct Menu_SwiftUI_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
